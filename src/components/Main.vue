80a2daac19f4be80f98c62b80fc64122c943ae6b
feffe <feffe@f-spexet.se>
2018-12-24 14:13:07 +0100

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="main">
+    <main-header/>
+    <router-view></router-view>
+  </div>
+</template>
+<script lang="ts">
+import Header from '@/components/MainHeader.vue';
+
+export default {
+  components: {
+    'main-header': Header,
+  },
+};
+</script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="top">
+    <router-link class="toplink" to="/biljetter">
+      <h1>Biljetter</h1>
+    </router-link>
+    <router-link class="toplink" to="/meny">
+      <h1>Meny</h1>
+    </router-link>
+    <router-link id="logo" to="/">
+      <img id="logo-image" src="@/assets/logo.png">
+    </router-link>
+    <router-link class="toplink" to>
+      <h1>Om oss</h1>
+    </router-link>
+    <router-link class="toplink" to>
+      <h1>Kontakt</h1>
+    </router-link>
+  </div>
+</template>
+
