b93de5919f00886e6e7ab08aaa5f53a87ade6d14
Oscar Carlsson <mr.oscar.carlsson@gmail.com>
2022-02-10 19:06:18 +0100

    Removed old news and added rebus for 2022

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,267 +1,34 @@
 <template>
-  <div class="contents">
-    <h1>Nyheter</h1>
-    <hr />
-      <div id="hostforestallningar">
-        <p>
-        Äntligen släpps höstens föreställningsdatum för F-spexet 2021: Rockefeller! Årets spex är ett familjedrama med karaktärer vars öden förändras för alltid efter ett oförutsett mord. Vi planerar att hålla två föreställningar:     
-        </p>
-<ul>
-<li> Lördagen 4:e september 18:00 </li>
-<li> Måndagen 6:e september 18:00 </li>
-</ul>
-<p>
-Föreställningarna kommer hållas fysiskt för studenter på Fysikteknologsektionen vid dessa tillfällen, för information om detta se <router-link to="/tickets">“Biljetter”</router-link>-sidan. Biljetter kommer gå att köpa för F-Nollan via ett biljettformulär som släpps under vecka 34. Om det finns lediga platser kvar den 2 september släpps dessa även för pHaddrar och övriga sektionsmedlemmar. Det kommer ingå en tvårätters middag i priset!
-</p>
-<p>
-Alla andra kan se spexet streamas vid samma tider via Youtube-kanalen F-spexet. (Följ den redan nu <a href="https://www.youtube.com/channel/UCtnqDd2Japmk7zmVAbHtK-Q" target="_blank">här</a>!). Mordmysteriet kommer inte vara mindre spännande digitalt och är dessutom helt GRATIS för alla! Tyvärr medför digitaliseringen att man inte kommer få någon mat serverad, men ni hittar ett tillhörande menyförslag på Meny-sidan om man vill laga sig en egen festmåltid. Vill man stötta F-spexet trots att man sitter hemma i soffan tar F-spexet också emot frivilliga donationer under föreställningens gång; mer detaljer för detta finns på <router-link to="/tickets">“Biljetter”</router-link>-sidan.
-<br /><br />
-        <span style="float:right"><i>2021-08-23</i></span> <br />
-</p>
-      </div>
-    <hr />
-	<div id="nyhetsbrev">
-	<p>
-		F-spexet har nu satt upp ett nyhetsbrev som kommer användas för att meddela när datum är släppta för nästa föreställningsperiod; vanligtvis sker detta två gånger per år. Om ni är intresserade kan ni fylla i nyhetsbrevsformuläret <a href="https://docs.google.com/forms/d/e/1FAIpQLSfKw-c3bX41CoAs7UmdRGWVR4QWLk4ntXaOWJo3zgKf-xETBg/viewform?usp=sf_link" target="_blank"    >här</a>.
-	</p>
-	</div>
-	<hr />
-	<div id="program">
-		<h2> Program</h2>
-	<p>
-	Nu har vi släppt programmet för årets spex. I programmet kan man hitta alla sångtexter, beskrivning av karaktärerna, beskrivning av menyn, citat från spexåret, och mer! Programmet hittar ni <a href="https://drive.google.com/file/d/1iE3uNw3MGVI4cKvQMpHy0PHD3D9_3Sev/view?usp=sharing" target="_blank"    >här</a>.
-	</p>
-	</div>
-	<hr />
-	<div id="trailer">
-		<h2> Trailer </h2>
-		<p>
-		F-spexet har gjort en trailer för årets spex och ni kan se den här eller direkt på YouTube. Mycket nöje!
-		</p>
-		<center>
-		<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/aNNRswSKuLM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>	
-		</center>
-	</div>
-	<hr />
-    <div id="datumslapp">
-	<h2> Datum är släppt! </h2>
-	<!--<div>
-		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfKw-c3bX41CoAs7UmdRGWVR4QWLk4ntXaOWJo3zgKf-xETBg/viewform?embedded=true" width="640" height="925" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-	</div>-->
-	<p class="font-weight-bold">
-	Äntligen släpps föreställningsdatum för F-spexet 2021: Rockefeller! Årets spex handlar om ett familjedrama med karaktärer vars öden förändras för alltid efter ett oförutsett mord. Vi planerar att hålla tre föreställningar:
-	</p>
-<ul>
-<li> premiär lördagen 15:e maj 17:00 </li>
-<li> fredagen 21:a maj  17:30 </li>
-<li> måndagen 7:e juni 17:00 </li>
-</ul>
-	
-	<p>
-	På grund av rådande omständigheter kommer året spex endast att streamas via Youtube på kanalen F-spexet. (Följ den redan nu <a href="https://www.youtube.com/channel/UCtnqDd2Japmk7zmVAbHtK-Q" target="_blank"    >här</a>!) Men oroa er inte! Mordmysteriet kommer inte vara mindre spännande och dessutom kommer föreställningarna att vara helt GRATIS för alla! För de som vill, kommer det dock finnas möjlighet att stödja F-spexet i form av frivilliga donationer innan samt under föreställningens gång; mer detaljer finns i <router-link to="/tickets">“Biljetter”</router-link>-sidan. Det innebär tyvärr även att vi inte kommer kunna servera mat som vanligt, men ett tillhörande menyförslag med lika god mat som det alltid har varit hittar ni på <router-link to="/menu">Meny</router-link>-sidan.
-	</p>
-        <div id="affisch-datumslapp">
-          <img src="../assets/affisch-datumslapp.png" class="big_image" alt>
-        </div>  
+    <div class="contents">
+        <h1>Nyheter</h1>
+            <hr />
+            <div id="rebus_2022">
+                <p>
+                    Årets rebus är här!
+                </p>
+                <div id="rebus_bild"> 
+                    <img src="../assets/rebus.png" class="big_image" alt>
+                </div>
+                <p>
+                    Lösningen ger titeln av årets uppsättning av F-spexet. Maila din
+                    gissning till <a href="mailto:rebus@f-spexet.se">rebus@f-spexet.se</a> senast 17 februari. Vi lottar ut några
+                    biljetter till föreställningen bland de som har gissat rätt. Alla som har
+                    gissat rätt blir även tillfrågade om de vill stå med i vårt program.
+                    Rätt svar presenteras på Focus under lunchen 18 februari samt här på vår hemsida
+                    och våra sociala medier (se sidfoten). Lycka till!
+                </p>
+
+                <br /><br />
+                <span style="float:right"><i style="color:White;">2022-02-10</i></span> <br />
+            </div>
+            <hr />
+            <div id="nyhetsbrev">
+                <p>
+                    F-spexet har nu satt upp ett nyhetsbrev som kommer användas för att meddela när datum är släppta för nästa föreställningsperiod; vanligtvis sker detta två gånger per år. Om ni är intresserade kan ni fylla i nyhetsbrevsformuläret <a href="https://docs.google.com/forms/d/e/1FAIpQLSfKw-c3bX41CoAs7UmdRGWVR4QWLk4ntXaOWJo3zgKf-xETBg/viewform?usp=sf_link" target="_blank">här</a>.
+                </p>
+            </div>
+        <hr />
     </div>
-    <hr />
-    <div id="temaslapp">
-      <h2> Temasläpp! </h2>
-        <p>
-          Hurra! F-spexet presenterar i år spexet Rockefeller:
-        </p>
-        <p>
-          John D Rockefeller har ägnat hela sin karriär åt att bygga sitt oljemonopol, förhindra konkurrens på den fria marknaden och att få sina barn att bli precis som han. Men en dag krossas den kapitalistiska idyllen plötsligt. Följ med på en hissnande mordgåta i tre akter, med tillhörande menyförslag för att förhöja upplevelsen! Föreställningsdatum släpps i april.
-        </p>
-        <div id="affisch">
-          <img src="../assets/affisch.png" class="big_image" alt>
-        </div>  
-    </div>
-    
-    <hr />
-    <div id="rebus">
-      <h2> Årets rebus släppt! </h2>
-      <p>
-        Äntligen är årets rebus släppt! 2021 må vara annorlunda mot tidigare år men F-spexet upprätthåller ändå sina traditioner!
-        <br />
-      </p>
-      <div id="rebus_bild"> 
-        <img src="../assets/rebus.png" class="big_image" alt>
-      </div>
-      <p>
-Lösningen ger titeln av årets uppsättning av F-spexet. Maila din gissning till <a href="mailto:rebus@f-spexet.se">rebus@f-spexet.se</a> senast 7 mars. Alla som har gissat rätt blir tillfrågade om de vill stå med i vårt program. Rätt svar presenteras bland annat på vår Facebook-sida under vecka 10.
-        <br /><br />
-        <span style="float:right"><i>2021-02-18</i></span> <br />
-      </p>
-    </div>
-    
-    
-    <hr />
-    <div id="news_aspiration">
-      <h2> Välkommen till ett nytt spexår! </h2>
-        <p>
-          Mottagningen på Chalmers har börjat, och ett vanligt år skulle det snart vara 
-          dags för höstföreställningar. Därför tycker vi att det är dags för en uppdatering om 
-          spexets framtidsplaner.
-        </p><p>
-          När vårföreställningarna ställdes in var vår ambition att sätta upp Johannes Kepler             under hösten istället. Tyvärr har Covid-19 hängt kvar längre än vad vi räknade med 
-          då, och vi kan inte med gott samvete sätta upp några höstföreställningar heller. 
-          Istället avslutar vi vårt verksamhetsår utan föreställningar och lägger Johannes 
-          Kepler på hyllan tills vidare.
-        </p>
-        <h3>
-          Betyder det här att allt vanligt spexande är lagt på is?
-        </h3>
-        <p> 
-          Självklart inte! Vi siktar på en nystart med ett nytt manus, som tills vidare går 
-          under arbetstemat <a>Totally Pink Batman</a>. Så länge inte pandemin stoppar oss 
-          sätts det upp någon gång under våren, med extra taggade spexare som sett fram i 
-          över ett år emot att få ha föreställningar. 
-        </p>
-        <h3>
-          Vill du inte bara se vårt nya spex, utan även vara med och sätta upp det? 
-        </h3>
-        <p>
-          Håll utkik efter information om aspningsaktiviteter, och gå med i spexet. Vi vill 
-          gärna spexa med just DIG!
-        </p><p>
-          Hoppas vi ses på aspningen och på de nya föreställningarna!
-        <br /><br />
-        <span style="float:right"><i>Styret för F-spexet 2020</i></span> <br />
-
-        </p>
-    </div>
-    
-    <!--
-      <h2>Spexets temasläppvisa</h2>
-      <h3>Text av:Media</h3>
-      <h3>Orginal melodi:Idas sommarvisa</h3>
-
-      <p>
-
-      Du ska inte tro det blir spex snart <br>
-      ifall inte rebus snart dyker upp <br>
-      och när våran rebus den dyker upp <br>
-      då vet du spexet är snart <br>
-
-
-      Du ska inte tro det blir sommar
-      Ifall inte nån sätter fart
-      På sommarn och gör lite somrigt
-      Då kommer blommorna snart
-      Jag gör så att blommorna blommar
-      Jag gör hela kohagen grön
-      Och nu så har sommaren kommit
-      För jag har just tagit bort snön
-
-      Jag gör mycket vatten i bäcken
-      Så där så det hoppar och far
-      Jag gör fullt med svalor som flyger
-      Och myggor som svalorna tar
-      Jag gör löven nya på träden
-      Och små fågelbon här och där
-      Jag gör himlen vacker om kvällen
-      För jag gör den alldeles skär
-
-      Och smultron det gör jag åt barna
-      För det tycker jag dom kan få
-      Och andra små roliga saker
-      Som passar när barna är små
-      Och jag gör så roliga ställen
-      Där barna kan springa omkring
-      Då blir barna fulla med sommar
-      Och bena blir fulla med spring
-
-        <router-link to="/tickets">
-          <button>Biljett</button>
-        </router-link>
-    	</p>
-    -->
-    
-    <!--
-    <hr />
-    <div id="news_cancellation">
-      <h2>Vårföreställningar inställda!</h2>
-      <p>
-        Det är med stor sorg och tungt hjärta som vi har beslutat att ställa in
-        samtliga föreställningar under våren. Som ni troligen är alltför väl
-        medvetna om, pågår för tillfället ett utbrott av
-        <span style="font-family:EB Garamond SC">covid-19</span> runt om i
-        världen. <span style="font-family:EB Garamond SC">who</span> har nyligen
-        klassat utbrottet som en pandemi, och regeringen har infört ett förbud
-        mot allmänna evenemang på över 500 personer. I samband med dessa
-        händelser har vi dömt att det mest lämpliga beslutet är att fördröja
-        detta års första uppsättning föreställningar, då vi inte kan garantera
-        tillräckligt med smittskydd för varken publik eller våra spexare.
-      </p>
-
-      <h3>För er som redan köpt en biljett</h3>
-      <p>
-        Om ni har köpt en biljett till vårens inställda föreställningar, har vi
-        skickat ett mail till kontaktpersonen i ert sällskap med information om
-        återbetalning. Har ni inte emottagit ett sådant mail, kontakta vänligen
-        <a href="mailto:kassor@f-spexet.se"> kassor@f-spexet.se </a>.
-      </p>
-      <p>
-        Har ni övriga frågor, är ni mycket välkomna att kontakta
-        <a href="mailto:styret@f-spexet.se"> styret@f-spexet.se </a>.
-      </p>
-
-      <h3>Kommer spexet att överleva?</h3>
-      <p>
-        <span style="font-family:EB Garamond SC">Ja!</span> Ni har inte sett det
-        sista av Johannes Kepler, och än mindre oss! I höst kommer vi tillbaka
-        med pompa och ståt, och vi hoppas få kunna se er då! Tills dess tackar
-        vi för er förståelse, och önskar er det bästa under dessa osäkra tider!
-        <br />
-        <span style="float:right"><i>F-spexet 2020</i></span> <br />
-      </p>
-    </div>
-
-    <hr />
-
-    <div id="news_temaslapp">
-      <h2>F-spexet 2020 &mdash; Johannes Kepler</h2>
-      <p style="margin-top: -20px">
-        <span style="font-family:EB Garamond SC">eller</span>
-        <span style="font-style:italic;font-feature-settings:'hlig'">
-          Illustrerad Vetenskap</span
-        >
-      </p>
-      <p>
-        Är er vardag trist och grå? Har ni alltid haft siktet inställt på
-        stjärnorna men aldrig riktigt haft budgeten att åka till Mars? Då har ni
-        mycket gemensamt med den unge Johannes Kepler som alltid har något i
-        kikaren, bokstavligen.
-      </p>
-      <p>
-        Någon gång i framtiden kommer vi framföra 2020 års fantastiska spex, som
-        tar oss till det Tysk-Romerska rikets hov i Prag kring sekelskiftet år
-        1600. Vi följer den unge Johannes Kepler som just ska träda in som
-        lärling till hovastronomen Tycho Brahe, efter att ha nått prominens i
-        Graz under sin mentor, Michael Mästlin.
-      </p>
-      <p></p>
-      <img src="../assets/mysterium.png" class="big_image" alt />
-    </div>
-
-    <hr />
-
-    <div id="news_rebus">
-      <h2>Årets rebus är här!</h2>
-      <p id="intro"></p>
-      <p>
-        Nu har temat släppts, men rebusen förblir. Kan du lyckas lista fram
-        <i>Johannes Kepler</i> från den här finurliga stofilen, med sin stav och
-        dopkärl?
-      </p>
-      <img
-        src="../assets/rebus.png"
-        class="big_image"
-        alt="Rebus för spexet 2020"
-      />
-    </div>-->
-  </div>
 </template>
 
 <style scoped>
