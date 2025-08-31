0b00b36e8c9235fb5b5b5b9d9d7a48016d02e991
TheCoder5550 <alfons.nilsson@outlook.com>
2025-08-31 18:20:49 +0200

    Add documentation for event.ts

--- src/event.ts
+++ src/event.ts
@@ -1,5 +1,8 @@
-// Året för nollan slår om typ 1 juli varje år (det är inte alltid 181 dagar från 1 jan till 1 juli men det stämmer typ)
-const nollanYear = new Date(new Date().valueOf() - 181 * 24 * 60 * 60 * 1000).getFullYear();
+/**
+ * Update this file with info about the Spex and it will
+ * be correctly updated and displayed everywhere on the
+ * website
+ */
 
 const KALLE_GLADER: SpexLocation = {
   name: "Kalle Glader",
@@ -13,6 +16,9 @@ const FOCUS: SpexLocation = {
   address: "Kemivägen 11, 412 58 Göteborg",
   preposition: "på"
 }
+
+// Året för nollan slår om typ 1 juli varje år (det är inte alltid 181 dagar från 1 jan till 1 juli men det stämmer typ)
+const nollanYear = new Date(new Date().valueOf() - 181 * 24 * 60 * 60 * 1000).getFullYear();
 
 const AUTUMN_EVENT: SpexEvent = {
   semester: "autumn",
@@ -63,21 +69,54 @@ const EVENT: SpexEvent = AUTUMN_EVENT;
 export default EVENT;
 
 export interface SpexEvent {
+  /**
+   * Set which semester the performances takes place to correcly
+   * display "höstens föreställningar" or "vårens föreställningar"
+   */
   semester: "spring" | "autumn";
+  /**
+   * Information about the tickets
+   */
   tickets: SpexTickets;
+  /**
+   * The location where all performances take place. (Note: It is not
+   * possible to have individual locations for every performance. TODO: Add
+   * functionality for this if you need it)
+   */
   location: SpexLocation;
+  /**
+   * List of days where there are performances
+   */
   performances: SpexPerformance[];
+  /**
+   * This note will be displayed below the table of dates on the ticket-page
+   */
   generalPerformanceNote?: string;
+  /**
+   * List of every price group
+   */
   prices: SpexPrice[];
 }
 
 export interface SpexTickets {
+  /**
+   * Link to a Google Form
+   */
   form_link: string;
 }
 
 export interface SpexLocation {
+  /**
+   * Display name of location
+   */
   name: string;
+  /**
+   * Link to the location on Google maps
+   */
   maps: string;
+  /**
+   * Human-readable address
+   */
   address?: string;
   /**
    * I stället för att skriva "framförs i Focus" går det att sätta
@@ -87,14 +126,35 @@ export interface SpexLocation {
 }
 
 export interface SpexPerformance {
+  /**
+   * Format: `yyyy-mm-dd`
+   */
   day: string;
+  /**
+   * Format: `hh:mm`
+   */
   time: string;
+  /**
+   * Note displayed under "Kommentar" column on ticket-page
+   */
   note?: string;
+  /**
+   * Set to `true` when tickets for this day are sold out
+   */
   sold_out?: boolean;
 }
 
 export interface SpexPrice {
+  /**
+   * Display name for this price group
+   */
   name: string;
+  /**
+   * Price in SEK
+   */
   price: number;
+  /**
+   * Footnote for this price group
+   */
   note?: string;
 }
