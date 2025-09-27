00439cb5f57515661a3c31356da44d683d1c0584
TheCoder5550 <alfons.nilsson@outlook.com>
2025-09-27 10:52:08 +0200

    Fixed favicon and removed old icons

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite';
 // import vue from '@vitejs/plugin-vue' // vue 3
 import vue from '@vitejs/plugin-vue2'; //vue 2
+import favicons from '@peterek/vite-plugin-favicons'
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    favicons('src/assets/favicon/favicon.png', {
+      appName: "F-Spexet",
+      appShortName: "F-Spexet",
+      appDescription: "Fysikteknologsektionens studentspex på Chalmers"
+    })
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  // File formats are case-sensative so we add JPG here
   assetsInclude: ['**/*.JPG'],
 })
